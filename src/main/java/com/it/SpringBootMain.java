b319a211aa780982529092a958e88eeb911df6cc
JavaHunk2020 <javahunk2020@gmail.com>
2024-03-02 06:19:52 +0530

    capability for service discovery client

--- src/main/java/com/it/SpringBootMain.java
+++ src/main/java/com/it/SpringBootMain.java
@@ -1,9 +1,8 @@
 package com.it;
 
-import java.util.Stack;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 
@@ -11,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringBootMain {
 
 	public static void main(String[] args) {
